931c9e0ab3c194fc91d9522b1a74ecbba9339464
BohdanBenovskyi <benovskiy1994@gmail.com>
2017-10-21 22:18:52 +0300

    add functions: allocMatrix, releaseMatrix, initMatrix, showMatrix

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -7,9 +7,52 @@
 using namespace std;
 
 
+void allocMatrix(int** &m, int n);
+void releaseMatrix(int** &m, int n);
+void initMatrix(int** &m, int n);
+void showMatrix(int** m, int n);
+
 int main()
 {
+	int n = 0;
+	cout << "Enter size of matrix: ";
+	cin >> n;
+	int **arr = NULL;
+
+	allocMatrix(arr, n);
+	initMatrix(arr, n);
+	showMatrix(arr, n);
+	releaseMatrix(arr, n);
+	
+
 	system("pause");
     return 0;
 }
 
+void allocMatrix(int** &m, int n) {
+	m = new int*[n];
+
+	for (int i = 0; i < n; i++)
+		m[i] = new int[n];
+}
+
+void releaseMatrix(int** &m, int n) {
+	for (int i = 0; i < n; i++)
+		delete[] m[i];
+
+	delete[] m;
+}
+
+void initMatrix(int** &m, int n) {
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			m[i][j] = rand() % 10;
+}
+
+void showMatrix(int** m, int n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+			cout << m[i][j] << "\t";
+		cout << "\n";
+	}
+}
