a1514debfa063fb65cd84c65f75fde5a73c24652
BohdanBenovskyi <benovskiy1994@gmail.com>
2017-10-22 14:56:09 +0300

    add header file and realization of header file

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -3,16 +3,9 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include "matrix.h"
 
 using namespace std;
-
-
-void allocMatrix(int** &m, int n);
-void releaseMatrix(int** &m, int n);
-void initMatrix(int** &m, int n);
-void showMatrix(int** m, int n);
-void showDiagonal(int** m, int n);
-void findMaxValue(int** m, int n);
 
 int main()
 {
@@ -31,57 +24,4 @@ int main()
 
 	system("pause");
     return 0;
-}
-
-void allocMatrix(int** &m, int n) {
-	m = new int*[n];
-
-	for (int i = 0; i < n; i++)
-		m[i] = new int[n];
-}
-
-void releaseMatrix(int** &m, int n) {
-	for (int i = 0; i < n; i++)
-		delete[] m[i];
-
-	delete[] m;
-}
-
-void initMatrix(int** &m, int n) {
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			m[i][j] = rand() % 100;
-}
-
-void showMatrix(int** m, int n) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++)
-			cout << m[i][j] << "\t";
-		cout << "\n";
-	}
-}
-
-void showDiagonal(int** m, int n) {
-	cout << "\n\n=======================================================\n\n";
-	for (int j = 0; j < n; j++) {
-		for (int i = 0; i < j + 1; i++) {
-			cout << m[j][i] << "\t";
-		}
-		cout << "\n";
-	}
-	cout << "\n=========================================================\n\n";
-}
-
-void findMaxValue(int** m, int n) {
-	int maxVal = 0;
-	for (int j = 0; j < n; j++) {
-		for (int i = 0; i < j + 1; i++) {
-			if (j == 0 && i == 0)
-				maxVal = m[j][i];
-			else
-				if(m[j][i] > maxVal)
-					maxVal = m[j][i];
-		}
-	}
-	cout << "Max value: " << maxVal << "\n";
 }
